d6f1358db7e7039cbe88f038815c6f6d3e454f79
mattg1243 <mattgallucci97@gmail.com>
2021-08-15 01:30:24 -0700

    functions good, debugging logs printing

--- script.js
+++ script.js
@@ -8,20 +8,82 @@ function playerPlay() {
     console.log("What do you pick, rock paper or scissors?")
 }
 
-function rps() {
+function rps(playerSelection, computerSelection) {
+
+    let winner;
 
     console.log(`The computer plays ${computerSelection}`)
 
-    if (computerSelection === 'paper') {
-        console.log("Sorry, you lost. Paper beats Rock!")
-    } else if (computerSelection === 'scissors') {
-        console.log("Congrats, you won. Rock beats Scissors!")
-    } else if (computerSelection === 'rock') {
-        console.log("Its a tie!")
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            console.log("Sorry, you lost. Paper beats Rock!")
+            winner = 'computer';
+        } else if (computerSelection === 'scissors') {
+            console.log("Congrats, you won. Rock beats Scissors!")
+            winner = 'player';
+        } else if (computerSelection === 'rock') {
+            console.log("Its a tie!")
+            winner = 'tie';
+        }
     }
+
+    if (playerSelection === 'paper') {
+        if (computerSelection === 'paper') {
+            console.log("Its a tie!")
+            winner = 'tie';
+        } else if (computerSelection === 'scissors') {
+            console.log("Sorry, you lost. Scissors beats Paper!")
+            winner = 'computer';
+        } else if (computerSelection === 'rock') {
+            console.log("Congrats, you won. Paper beats Rock!")
+            winner = 'player';
+        }
+    }
+
+    if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            console.log("Congrats, you won. Scissor beats Paper!")
+            winner = 'player';
+        } else if (computerSelection === 'scissors') {
+            console.log("Its a tie!")
+            winner = 'tie';
+        } else if (computerSelection === 'rock') {
+            console.log("Sorry, you lost. Rock beats Scissors!")
+            winner = 'computer';
+        }
+    }
+
+    return winner
 }
 
-const playerSelection = 'rock';
-const computerSelection = computerPlay();
-console.log(rps())
 
+function game() {
+
+    let playerScore = 0, computerScore = 0, ties = 0;     
+    
+    for (let i=0; i < 5; i++) {
+        
+        let computerSelection = computerPlay();
+        let playerSelection = prompt('Make a move: rock, paper, or scissors: ');
+        console.log(computerSelection);
+        let roundWinner = rps(computerSelection, playerSelection);
+        console.log("roundWinner = " + roundWinner);
+        
+        if (roundWinner === 'computer') {
+            computerScore++;
+        } else if (roundWinner === 'player') {
+            playerScore++;
+        } else if (roundWinner === 'tie') {
+            ties++;
+        }
+    }
+
+    if (playerScore > computerScore) {
+        console.log('You won the game!') 
+    } else {
+        console.log('Sorry, better luck next time.')
+    }
+    console.log(`The score was player: ${playerScore} computer: ${computerScore} with ${ties} ties.`)
+}
+
+game();
